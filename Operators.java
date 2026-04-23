37b9d4504fdd9e186a92cdfcc75d74d2c86fc579
Your Name <your_email@example.com>
2026-04-23 23:48:29 +0530

    "AllOperators"

---
+++ Operators.java
@@ -0,0 +1,17 @@
+public class Operators {
+     public static void main(String [] args)
+        {
+
+        //variable
+        int a=10;
+        int b=20;
+        int sum=a+b;
+
+        System.out.println("sum: "+sum);//30
+        System.out.println(a-2);//8
+        System.out.println(4*b);//80
+        System.out.println(a/b);
+        System.out.println(a%b);
+        }
+    
+}
