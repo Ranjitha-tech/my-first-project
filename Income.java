29d0f230a7d1b2c95dc73efbf7c142188f534186
Your Name <your_email@example.com>
2026-04-29 17:07:40 +0530

    Added income program

---
+++ Income.java
@@ -0,0 +1,15 @@
+class Income{
+    public static void main(String[]args)
+    {
+        int income=1000;
+
+        if(income>7000)
+        {
+            System.out.println("Scholarship Available");
+        }
+        else
+        {
+            System.out.println("Not eligible for scholarship");
+        }
+    }
+}
