fe3fd5d412b93c7905c347b2264a462040b1a05b
Your Name <your_email@example.com>
2026-05-06 17:24:34 +0530

    to check person's gender

---
+++ Gender.java
@@ -0,0 +1,19 @@
+class Gender{
+
+    public static void main(String[]args)
+    {
+        String gender="Female";
+        if(gender=="male")
+        {
+            System.out.println("male");
+        }
+        else if(gender=="Female")
+        {
+            System.out.println("Female");
+        }
+        else
+        {
+            System.out.println("others");
+        }
+    }
+}
